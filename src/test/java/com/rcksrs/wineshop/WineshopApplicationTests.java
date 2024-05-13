d86181c95242463f935cf87da7300a0c503b78dc
Eryck Soares <rcksrs@outlook.com>
2024-05-13 19:39:41 -0300

    initial commit

---
+++ src/test/java/com/rcksrs/wineshop/WineshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rcksrs.wineshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WineshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
